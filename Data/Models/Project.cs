a628a51ea924361e846a650539c684c273e4d41f
Sama-wahidee <118561833+Sama-wahidee@users.noreply.github.com>
2024-08-15 07:57:23 +0300

    Sama Test

--- Data/Models/Project.cs
+++ Data/Models/Project.cs
@@ -6,6 +6,7 @@ using System.Net.Mail;
 
 namespace Project.Data.Model
 {
+    //SamaComment
     public class Project
     {
         [Key]
--- Data/Models/Waterfall.cs
+++ Data/Models/Waterfall.cs
@@ -2,6 +2,7 @@
 {
     public class Waterfall : Project
     {
+        //Sama Test
         public DateTime ExpectedEndTime { get; set; }
         public ICollection<Task> Tasks { get; set; }
     }
