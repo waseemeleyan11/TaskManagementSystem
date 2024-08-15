4dc2d892cffcfeb62f84defd8b7bdf58c5bc7058
waseem eleyan <wm1210262@gmail.com>
2024-08-15 20:14:29 +0300

    wes
    add comment and taske

---
+++ Data/Models/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project.Model
+{
+    public class Comment
+    {
+        [Key]
+        public int CommentId { get; set; }
+        public string CommentText { get; set; }
+        public int MinLength { get; set; }
+        public int MaxLength { get; set; }
+        public string Type { get; set; }
+        public bool IsDeleted { get; set; }
+        [ForeignKey(nameof(Task))]
+        public int TaskId { get; set; }
+        public Task Task { get; set; }
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+
+}
