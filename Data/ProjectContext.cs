08312dbc78a164df32bb12b410f52ad8e999b1c7
waseem eleyan <wm1210262@gmail.com>
2024-08-19 10:26:51 +0300

    c

--- Data/ProjectContext.cs
+++ Data/ProjectContext.cs
@@ -1,93 +1,98 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using TaskManagementSystem.Data.Models;
+using Task = TaskManagementSystem.Data.Models.Task;
 namespace TaskManagementSystem.Data
 {
-    using TaskManagementSystem.Data.Models;
-
-    public class ProjectContext :DbContext
+    public class ProjectContext : DbContext
     {
-        IConfiguration configuration;
+        private readonly IConfiguration _configuration;
+
         public ProjectContext(IConfiguration configuration)
         {
-            this.configuration = configuration;
+            _configuration = configuration;
         }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DefaultConnection"));
         }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            // User-Project (Many-to-Many) Configuration
+            modelBuilder.Entity<ProjectUser>()
+                .HasKey(pu => new { pu.userId, pu.projectId });
+
+            modelBuilder.Entity<ProjectUser>()
+                .HasOne(pu => pu.User)
+                .WithMany(u => u.ProjectUsers)
+                .HasForeignKey(pu => pu.UserId);
+
+            modelBuilder.Entity<ProjectUser>()
+                .HasOne(pu => pu.Project)
+                .WithMany(p => p.projectUsers)
+                .HasForeignKey(pu => pu.projectId);
+
+            // User-Task (Many-to-Many) Configuration
+            modelBuilder.Entity<UserTask>()
+                .HasKey(ut => new { ut.UserId, ut.TaskId });
+
+            modelBuilder.Entity<UserTask>()
+                        .HasOne(ut => ut.Users)
+                        .WithMany(u => u.UserTasks)
+                        .HasForeignKey(ut => ut.UserId);
+
+            modelBuilder.Entity<UserTask>()
+                .HasOne(ut => ut.Tasks)
+                .WithMany(t => t.UserTasks)
+                .HasForeignKey(ut => ut.TaskId);
+
+            // User-Comment (One-to-Many) Configuration
             modelBuilder.Entity<User>()
-            .HasOne(u => u.Attachment)          // Each User has one Attachment
-            .WithOne(a => a.User)               // Each Attachment has one User
-            .HasForeignKey<User>(u => u.AttachmentId) // Foreign key in User table
-            .OnDelete(DeleteBehavior.Cascade);
+                .HasMany(u => u.Comments)
+                .WithOne(c => c.User)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-            //modelBuilder.Entity<User>().HasMany(u => u.Projects)
-              //  .WithOne(p => p.User).HasForeignKey(p => p.userId).OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<User>()
-            .HasMany(u => u.Comments)    
-            .WithOne(c => c.User)       // Each Comment has one User
-            .HasForeignKey(c => c.UserId)  // Foreign key in Comment table
-            .OnDelete(DeleteBehavior.Cascade);
-
-            modelBuilder.Entity<User>()
-            .HasMany(u => u.Tasks)    // A User has many Tasks
-            .WithOne(t => t.User)    // Each Task has one User
-            .HasForeignKey(t => t.UserId)  // Foreign key in Task table
-            .OnDelete(DeleteBehavior.Cascade);
-
+            // Task-Comment (One-to-Many) Configuration
             modelBuilder.Entity<Task>()
-           .HasMany(t => t.Comments)    // A Task has many Comments
-           .WithOne(c => c.Task)       // Each Comment has one Task
-           .HasForeignKey(c => c.TaskId)  // Foreign key in Comment table
-           .OnDelete(DeleteBehavior.Cascade);
+                .HasMany(t => t.Comments)
+                .WithOne(c => c.Task)
+                .HasForeignKey(c => c.TaskId)
+                .OnDelete(DeleteBehavior.Cascade);
 
+            // User-Sprint (One-to-Many) Configuration
             modelBuilder.Entity<User>()
-           .HasMany(u => u.Sprints)
-           .WithOne(c => c.User)       // Each Comment has one User
-           .HasForeignKey(c => c.UserId)  // Foreign key in Comment table
-           .OnDelete(DeleteBehavior.Cascade);
+                .HasMany(u => u.Sprints)
+                .WithOne(s => s.User)
+                .HasForeignKey(s => s.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<User>()
-           .HasOne(t => t.Agile)    // A Task has many Comments
-           .WithOne(c => c.User)       // Each Comment has one Task
-           .HasForeignKey<Agile>(c => c.userId)  // Foreign key in Comment table
-           .OnDelete(DeleteBehavior.Cascade);
-
+            // Agile-Sprint (One-to-Many) Configuration
             modelBuilder.Entity<Agile>()
-            .HasMany(u => u.Sprints)    // A User has many Tasks
-            .WithOne(t => t.Agile)    // Each Task has one User
-            .HasForeignKey(t => t.AgileId)  // Foreign key in Task table
-            .OnDelete(DeleteBehavior.Cascade);
+                .HasMany(a => a.Sprints)
+                .WithOne(s => s.Agile)
+                .HasForeignKey(s => s.AgileId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-            /////
+            // User-Attachment (One-to-One) Configuration
             modelBuilder.Entity<User>()
-           .HasMany(u => u.Tasks)
-           .WithOne(c => c.User)       // Each Comment has one User
-           .HasForeignKey(c => c.UserId)  // Foreign key in Comment table
-           .OnDelete(DeleteBehavior.Cascade);
+                .HasOne(u => u.Attachment)
+                .WithOne(a => a.User)
+                .HasForeignKey<User>(u => u.AttachmentId)
+                .OnDelete(DeleteBehavior.Cascade);
 
-            modelBuilder.Entity<User>()
-           .HasOne(t => t.Waterfall)    // A Task has many Comments
-           .WithOne(c => c.User)       // Each Comment has one Task
-           .HasForeignKey<Waterfall>(c => c.userId)  // Foreign key in Comment table
-           .OnDelete(DeleteBehavior.Cascade);
-
+            // Waterfall-Task (One-to-Many) Configuration
             modelBuilder.Entity<Waterfall>()
-            .HasMany(u => u.Tasks)    // A User has many Tasks
-            .WithOne(t => t.Waterfall)    // Each Task has one User
-            .HasForeignKey(t => t.ProjectId)  // Foreign key in Task table
-            .OnDelete(DeleteBehavior.Cascade);
-
-
-
-
+                .HasMany(w => w.Tasks)
+                .WithOne(t => t.Waterfall)
+                .HasForeignKey(t => t.WaterfallId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
 
+        // DbSets for the 10 Models
         public DbSet<Project> Projects { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<ProjectUser> ProjectUsers { get; set; }
@@ -98,10 +103,5 @@ namespace TaskManagementSystem.Data
         public DbSet<Attachment> Attachments { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<UserTask> UserTasks { get; set; }
-
-
-
-
-
     }
 }
